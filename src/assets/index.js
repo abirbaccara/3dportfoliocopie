caab0d6e13eb4592ce5d1749173cd29134f4b970
abirbaccara <abirbaccara96@gmail.com>
2023-05-26 17:17:21 +0100

    3D Portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import ubuntu from "./tech/ubuntu.png";
+import motokodfinity from "./tech/motokodfinity.png";
+import python from "./tech/python.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import bootstrap from "./tech/bootstrap.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import scale from "./company/scale.png";
+import security from "./company/security.png";
+import mihai_eminescu from "./company/mihai_eminescu.png";
+import online_library from "./company/online_library.png";
+
+import NFTMarketplace from "./NFTMarketplace.png";
+import personalportfolio from "./personalportfolio.png";
+import DBank from "./DBank.png";
+import Token from "./Token.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  ubuntu,
+  motokodfinity,
+  python,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  bootstrap,
+  tailwind,
+  typescript,
+  scale,
+  security,
+  mihai_eminescu,
+  online_library,
+  NFTMarketplace,
+  personalportfolio,
+  DBank,
+  Token,
+};
