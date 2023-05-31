844319c37c2691803986ba1730c2a2a0e5bbc864
abirbaccara <abirbaccara96@gmail.com>
2023-06-01 00:46:34 +0100

    3d_portfolio_copie

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about" id="about">
+        <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div 
               animate={{
@@ -40,4 +40,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
